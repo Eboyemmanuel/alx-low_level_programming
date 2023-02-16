3874ffe78c69924088e69a8118fe9b9ad9579f91
Eboyemmanuel <eboyemmanuel@gmail.com>
2023-02-16 13:38:14 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  *main - print "Programming is like building a multilingual puzzle, followed by a new line."
- *Return: Always 0 (Success).
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    
     return(0);
 }    
